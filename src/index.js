df863b079646bf2f2bf93c6cff3277f87d3768bb
Your Name <mareliza16@hotmail.com>
2022-01-25 09:58:50 -0500

    se realizo codigo en javascript,donde los valosres que tiene el input se ingresan en un array,se crea un ciclo for y condicional.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+function ClifillArray() {
+  let arrayInput = new Array();
+  arrayInput = document.getElementById("cardnumber").value;
+  console.log("el dato es:" + arrayInput);
+  console.log("el dato es:" + arrayInput[0]);
+
+  for (let i = 0; i <= arrayInput.length; i++) {
+    if (i % 2 == 0) {
+      let pares = arrayInput.push(i);
+      console.log("los números pares son:" + pares);
+    } else {
+      console.log("los números impares son:" + arrayInput);
+    }
+  }
+}
+
+let buttonValidate = document.getElementById("validate");
+buttonValidate.addEventListener("click", ClifillArray);
