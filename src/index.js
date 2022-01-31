e64443d4ae265395af3cd5cad5ede95e00ffb4e1
Your Name <mareliza16@hotmail.com>
2022-01-31 13:21:05 -0500

    termine las funciones de isvalid y maskifi en javascript y empece scc.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import validator from "./validator.js";
 
-console.log(validator);
+//validator.maskify;
 
 function ClifillArray() {
   let arrayInput = new Array();
   arrayInput = document.getElementById("cardnumber").value;
-  console.log("el dato es:" + arrayInput);
-  console.log("el dato es:" + arrayInput[0]);
+  validator.isValid(arrayInput);
 
-  for (let i = 0; i <= arrayInput.length; i++) {
-    if (i % 2 == 0) {
-      let pares = i * 2;
-      //arrayInput.push(pares);
-      console.log("los números pares son:" + pares);
-    } else {
-      //console.log("los números impares son:" + arrayInput[i]);
-    }
-  }
+  document.getElementById("cardnumber").value = validator.maskify(arrayInput);
+
+  console.log(validator);
 }
-
 let buttonValidate = document.getElementById("validate");
 buttonValidate.addEventListener("click", ClifillArray);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,67 @@
 const validator = {
-  // ...
-};
+  maskify: function (arrayInput) {
+    const ultimoscuatrosnumeros = arrayInput.substr(-4);
+    const primerosNumerosTarjeta = arrayInput
+      .substr(1, arrayInput.length - 5)
+      .replace(/\d/g, "#");
+    console.log(`${primerosNumerosTarjeta}${ultimoscuatrosnumeros}`);
+    return ` ${primerosNumerosTarjeta}${ultimoscuatrosnumeros}`;
+  },
+  isValid: function (arrayInput) {
+    let confirmacion = false;
+    let arrayInvertido = new Array();
 
+    for (let i = arrayInput.length - 1; i >= 0; i--) {
+      arrayInvertido.push(arrayInput[i]);
+    }
+
+    let sumaIndicesPar = new Array();
+
+    for (let i = 0; i < arrayInvertido.length; i++) {
+      if ((i + 1) % 2 === 0) {
+        let multiplicacionPar = arrayInvertido[i] * 2;
+
+        if (multiplicacionPar >= 10) {
+          let arrayMultiplicacionPar = String(multiplicacionPar).split("");
+          /*el + es para convertir los valores del vector en números ya que en arrayMultiplicacionPar los tenemos como string*/
+          let sumaParejasPares =
+            +arrayMultiplicacionPar[0] + +arrayMultiplicacionPar[1];
+          sumaIndicesPar.push(sumaParejasPares);
+          /* console.log(
+            "los mayores y iguales a 10 que se suman" + sumaIndicesPar
+          );*/
+        } else {
+          sumaIndicesPar.push(multiplicacionPar);
+          /* console.log(
+            "los valores que son menores a 10 y son par " + sumaIndicesPar
+          );*/
+        }
+      } else {
+        sumaIndicesPar.push(arrayInvertido[i]);
+        /*console.log(
+          "los valores que son menores a 10 y son impar" + sumaIndicesPar
+        );*/
+      }
+    }
+
+    console.log(
+      "los valores que son menores a 10 y son impar" + sumaIndicesPar
+    );
+    let sumaArray = 0;
+
+    for (let i = 0; i < sumaIndicesPar.length; i++) {
+      sumaArray += +sumaIndicesPar[i];
+    }
+    //console.log("la suma del vector es " + sumaArray);
+    if (sumaArray % 10 === 0) {
+      alert("gracias por su compra,lo esperamos pronto");
+      return confirmacion === true;
+    } else {
+      alert(
+        "Lo sentimos tu tarjeta no es valida,por favor ingresa un nuevo número"
+      );
+      return confirmacion;
+    }
+  },
+};
 export default validator;
